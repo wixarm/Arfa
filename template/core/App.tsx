439c5264e653183a86423cb3b2fcb10b85a66369
wixarm <armantarhani1997@gmail.com>
2025-08-08 13:32:59 +0330

    modernize template structure

--- template/core/App.tsx
+++ template/core/App.tsx
@@ -2,9 +2,12 @@ import { h, Fragment } from "./runtime/jsx";
 import "./styles.css";
 
 export default function App() {
+  const handleAlert = () => {
+    alert("hello!");
+  };
   return (
     <>
-      <h1 class="test" onClick={() => alert("test")}>
+      <h1 class="test" onClick={handleAlert}>
         Welcome to Arfa App
       </h1>
       <div>test</div>
--- template/core/runtime/jsx.ts
+++ template/core/runtime/jsx.ts
@@ -1,15 +1,10 @@
-// src/runtime/jsx.ts
-
 export function h(
   type: string | Function,
   props: Record<string, any> | null,
   ...children: any[]
 ) {
-  // default props object
   props = props ? { ...props } : {};
-  // flatten & drop null/undefined
   const flat = children.flat().filter((c) => c != null);
-  // if single child, don’t wrap in array
   props.children = flat.length === 1 ? flat[0] : flat;
   return { type, props };
 }
